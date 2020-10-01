cd231038bdd57d4d331ede444e79fdb059a27d23
ysumeet <s3797892@student.rmit.edu.au>
2020-10-01 22:03:57 +1000

    sprin 2 files commit

--- test/unit/stafflistTest.php
+++ test/unit/stafflistTest.php
@@ -1,7 +1,8 @@
 <?php
-include "../product/login.php";
-class loginTest extends \PHPUnit\Framework\TestCase{
-public function testValidationOk()
+include "../product/stafflist.php";
+class stafflistTest extends \PHPUnit\Framework\TestCase{
+    /** @test */
+public function ValidationOk()
     {   $_POST['Usernames'] = "admin";
         $_REQUEST['Passwords'] = "admin";
    
